6e0666e33350337c54e8384a6b44f022e980bb01
Aman-codde <amandeepk221@gmail.com>
2023-02-01 13:57:03 -0800

    feat(): split into small components

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, FlatList } from "react-native";
+import GoalInput from "./componenets/GoalInput";
+import GoalItem from "./componenets/GoalItem";
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function addGoalHandler(enteredGoalText) {
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
+    ]);
+  }
+
+  function deleteGoalHandler() {
+    console.log("delete goal");
+  }
+
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Enter your goal" />
-        <Button title="Add Goal" />
-      </View>
-      <View>
-        <Text>List of goals...</Text>
+      <GoalInput onAddGoal={addGoalHandler}></GoalInput>
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={courseGoals}
+          renderItem={(data) => {
+            return (
+              <GoalItem
+                text={data.item.text}
+                onDeleteGoal={deleteGoalHandler}
+              />
+            );
+          }}
+          keyExtractor={(item, index) => item.id}
+          alwaysBounceVertical={false}
+        ></FlatList>
       </View>
     </View>
   );
@@ -17,6 +41,11 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
